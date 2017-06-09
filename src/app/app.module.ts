dea9bfc466c30a0596ad4b5bd8103726449c43e3
rolandoDev <rolando.sanchez.developer@gmail.com>
2017-06-09 12:28:12 -0400

    1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,21 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from '@ionic/storage';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+export const firebaseConfig={
+  apiKey: "AIzaSyAN7xztWAqc6SeItWKYUYLPaB-VIG3QBgs",
+  authDomain: "labs-fd4c0.firebaseapp.com",
+  databaseURL: "https://labs-fd4c0.firebaseio.com",
+  projectId: "labs-fd4c0",
+  storageBucket: "labs-fd4c0.appspot.com",
+  messagingSenderId: "1062615951835"
+}
 
 @NgModule({
   declarations: [
@@ -14,7 +26,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,80 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, AlertController, LoadingController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { AngularFireDatabase,FirebaseListObservable } from "angularfire2/database";
+import 'rxjs/add/operator/map';
+import firebase from 'firebase/app'
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  nick:string;
+  message:string;
+  messages:Array<any>;
+  constructor(private navCtrl: NavController,private alertCtrl: AlertController,private storage: Storage,public loadingCtrl: LoadingController, private db: AngularFireDatabase) {
 
   }
+  ionViewDidLoad() {
+    let loading = this.loadingCtrl.create({
+      content: 'Cargando...'
+    });
+    loading.present();
+    this.storage.get('nick').then(nick=>{
+      if(!nick)this.showPrompt();
+      else this.nick=nick;
+      loading.dismiss();
+    })
+    this.getMessages()
+  }
+  showPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'NICK',
+      message: "Escribe tu nick para comenzar a chatear.",
+      inputs: [
+        {
+          value: this.nick,
+          name: 'nick',
+          placeholder: 'Nick'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Guardar',
+          handler: data => {
+            let inputNick:string=data.nick;
+            if(inputNick.length>2 && inputNick.length<20){
+              this.storage.set('nick',inputNick.toLocaleLowerCase()).then(res=>{
+                this.nick=inputNick.toLocaleLowerCase();
+              });
+            }
+            else return false;
+          }
+        }
+      ],
+      enableBackdropDismiss:false
+    });
+    prompt.present();
+  }
+  send(){
 
+    let message={
+      text: this.message,
+      nick: this.nick,
+      timestamp: firebase.database.ServerValue.TIMESTAMP
+    }
+    this.messages.push(message);
+    this.message=null;
+  }
+  getMessages(){
+    console.log('GET');
+    
+   this.db.list('/messages').subscribe(messages=>{
+      this.messages=messages.sort((a,b)=>{
+        if(a<b) return -1;
+        else return 1;
+      })
+    });
+
+  }
 }
